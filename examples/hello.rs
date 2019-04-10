1700be6182f72119c0acfa17a9acf682f8c9c792
Willem Victor <wimpievictor@gmail.com>
2019-04-10 21:34:58 +0200

    Cleaned up warnings

--- examples/hello.rs
+++ examples/hello.rs
@@ -3,7 +3,7 @@ extern crate rengine;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut app = rengine::AppBuilder::new()
+    let app = rengine::AppBuilder::new()
         .title("Hello Example")
         .size(800, 600)
         .background_color([0.3, 0.4, 0.5, 1.0])
