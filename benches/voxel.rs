5222ae8cb5ea0b7e4397276a705944974fa96af5
Willem Victor <wimpievictor@gmail.com>
2019-05-07 21:54:39 +0200

    Benchmarking boilerplate

---
+++ benches/voxel.rs
@@ -0,0 +1,13 @@
+
+#[macro_use]
+extern crate criterion;
+extern crate rengine;
+
+use criterion::Criterion;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("voxel iteration", |b| b.iter(|| {}));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
