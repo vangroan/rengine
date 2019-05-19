d24718860171b02b54454b1c060c54ea8b705a83
Willem Victor <wimpievictor@gmail.com>
2019-05-19 09:34:29 +0200

    Fixed broken test

--- tests/chunks.rs
+++ tests/chunks.rs
@@ -1,16 +1,21 @@
 extern crate rengine;
 
-use rengine::specs::{Builder, World};
-use rengine::voxel::{ChunkControl, ChunkUpkeepSystem, VoxelArrayChunk, VoxelBoxGen, VoxelChunk};
+use rengine::comp::MeshCommandBuffer;
+use rengine::specs::{Builder, RunNow, SystemData, World};
+use rengine::voxel::{
+    ChunkControl, ChunkMapping, ChunkUpkeepSystem, NoOpVoxelMeshGen, VoxelArrayChunk, VoxelChunk,
+};
 
 type IntVoxel = u16;
 type IntVoxelChunk = VoxelArrayChunk<IntVoxel>;
-type IntUpkeepSystem = ChunkUpkeepSystem<IntVoxel, IntVoxelChunk, VoxelBoxGen>;
+type IntChunkCtrl = ChunkControl<u16, IntVoxelChunk>;
+type IntUpkeepSystem = ChunkUpkeepSystem<IntVoxel, IntVoxelChunk, NoOpVoxelMeshGen>;
 
 /// Ensure update queue is drained on maintain
 #[test]
 fn test_lazy_update() {
-    let mut ctrl: ChunkControl<u16, IntVoxelChunk> = Default::default();
+    let mut chunk_map = ChunkMapping::new();
+    let mut ctrl: IntChunkCtrl = Default::default();
     ctrl.lazy_update([0, 0, 0], 1);
     ctrl.lazy_update([1, 0, 0], 2);
     ctrl.lazy_update([2, 0, 0], 3);
@@ -19,19 +24,29 @@ fn test_lazy_update() {
     // ECS integration
     let mut world = World::new();
     world.register::<IntVoxelChunk>();
-    world.register();
     let entity = world
         .create_entity()
         .with(IntVoxelChunk::new([0, 0, 0]))
         .build();
-    ctrl.add_chunk(
+    chunk_map.add_chunk(
         entity,
-        world.read_storage::<IntVoxelChunk>().get(entity).unwrap(),
+        world
+            .read_storage::<IntVoxelChunk>()
+            .get(entity)
+            .unwrap()
+            .index()
+            .clone(),
     );
+    world.add_resource(ctrl);
+    world.add_resource(chunk_map);
+    world.add_resource(MeshCommandBuffer::new());
+
+    // Systems
+    let mut upkeep_system: IntUpkeepSystem = IntUpkeepSystem::new(NoOpVoxelMeshGen);
 
     // Maintain
-    ctrl.maintain(&mut world.write_storage::<IntVoxelChunk>());
-    assert_eq!(0, ctrl.cmd_len());
+    upkeep_system.run_now(&mut world.res);
+    assert_eq!(0, world.read_resource::<IntChunkCtrl>().cmd_len());
 
     {
         let chunks = world.read_storage::<IntVoxelChunk>();
