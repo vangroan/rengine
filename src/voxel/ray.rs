ed3f9daac8aadf08cae2c9f4280d589ddde1dffa
Willem Victor <wimpievictor@gmail.com>
2019-05-21 19:53:42 +0200

    Casting ray

--- src/voxel/ray.rs
+++ src/voxel/ray.rs
@@ -1,16 +1,22 @@
 use nalgebra::{Point3, Unit, Vector3};
-use std::f32::INFINITY;
+use std::f32::MAX;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct VoxelRayInfo;
 
 // https://lodev.org/cgtutor/raycasting.html
 #[allow(dead_code)]
-fn voxel_raycast<P, V>(
+fn voxel_raycast(
     origin: Point3<f32>,
     direction: Unit<Vector3<f32>>,
     steps: usize,
 ) -> Option<VoxelRayInfo> {
+    let (mut x, mut y, mut z) = (
+        origin.x.floor() as i32,
+        origin.y.floor() as i32,
+        origin.z.floor() as i32,
+    );
+
     // The length along the ray we need to travel to
     // cross a voxel border along a specific axis.
     //
@@ -19,42 +25,72 @@ fn voxel_raycast<P, V>(
     let delta_x = if direction.x != 0.0 {
         (1.0 / direction.x).abs()
     } else {
-        INFINITY
+        MAX
     };
 
     let delta_y = if direction.y != 0.0 {
         (1.0 / direction.y).abs()
     } else {
-        INFINITY
+        MAX
     };
 
     let delta_z = if direction.z != 0.0 {
         (1.0 / direction.z).abs()
     } else {
-        INFINITY
+        MAX
     };
+
+    println!("delta ({}, {}, {})", delta_x, delta_y, delta_z);
 
     // Determine which direction we are stepping.
     //
     // Calculate initial lengths from origin
     // to first crossing of boundries.
-    let (step_x, max_x) = if direction.x > 0.0 { 
+    let (step_x, mut max_x) = if direction.x > 0.0 {
         (1, (origin.x.ceil() - origin.x) * delta_x)
-    } else { 
+    } else {
         (-1, (origin.x.floor() - origin.x) * delta_x)
     };
 
-    let (step_y, max_y) = if direction.y > 0.0 { 
+    let (step_y, mut max_y) = if direction.y > 0.0 {
         (1, (origin.y.ceil() - origin.y) * delta_y)
-    } else { 
+    } else {
         (-1, (origin.y.floor() - origin.y) * delta_y)
     };
 
-    let (step_z, max_z) = if direction.z > 0.0 { 
+    let (step_z, mut max_z) = if direction.z > 0.0 {
         (1, (origin.z.ceil() - origin.z) * delta_z)
-    } else { 
+    } else {
         (-1, (origin.z.floor() - origin.z) * delta_z)
     };
+
+    println!("step ({}, {}, {})", step_x, step_y, step_z);
+    println!("t ({}, {}, {})", max_x, max_y, max_y);
+
+    let mut i = 0;
+    while i < steps {
+        println!("({}, {}, {}) t ({}, {}, {})", x, y, z, max_x, max_y, max_z);
+
+        if max_x < max_y {
+            if max_x < max_z {
+                max_x += delta_x;
+                x += step_x;
+            } else {
+                max_z += delta_z;
+                z += step_z;
+            }
+        } else {
+            if max_y < max_z {
+                max_y += delta_y;
+                y += step_y;
+            } else {
+                max_z += delta_z;
+                z += step_z;
+            }
+        }
+
+        i += 1;
+    }
 
     None
 }
@@ -66,8 +102,8 @@ mod test {
     #[test]
     fn test_basic_cast() {
         let info = voxel_raycast(
-            [0., 0., 0.].into(),
-            Unit::new_normalize([0.5, 0.25, 0.0].into()),
+            [0.5, 0.5, 0.1].into(),
+            Unit::new_normalize([0.5, 0.866, 0.0].into()),
             10,
         );
         assert_eq!(None, info);
