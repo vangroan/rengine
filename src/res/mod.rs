7132e7221f8501a6e63679bddac9296116d06963
Willem Victor <wimpievictor@gmail.com>
2019-08-17 00:37:30 +0200

    Removed unused signal type

--- src/res/mod.rs
+++ src/res/mod.rs
@@ -3,11 +3,9 @@
 mod assets;
 mod delta_time;
 mod device_dim;
-mod signal;
 mod view_port;
 
 pub use assets::*;
 pub use delta_time::*;
 pub use device_dim::*;
-pub use signal::*;
 pub use view_port::*;
