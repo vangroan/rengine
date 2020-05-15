467d2617da76dc0748d8e79b6251d2f10eeea30d
Willem Victor <wimpievictor@gmail.com>
2020-05-15 19:14:31 +0200

    Add aabb intersect check

--- src/gui/aabb.rs
+++ src/gui/aabb.rs
@@ -1,4 +1,5 @@
 use glutin::dpi::LogicalSize;
+use nalgebra::Point2;
 use specs::{Component, DenseVecStorage};
 
 /// Axis-aligned bounding box in logical pixel size.
@@ -19,5 +20,27 @@ impl WidgetBounds {
             width: size.width as f32,
             height: size.height as f32,
         }
+    }
+
+    /// Returns whether the given point is within the local
+    /// bounds, in logical pixels.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rengine::gui::WidgetBounds;
+    /// use nalgebra::Point2;
+    ///
+    /// let aabb = WidgetBounds::new(120.0, 70.0);
+    /// assert!(aabb.intersect_point([50.0, 50.0]));
+    /// assert!(!aabb.intersect_point([400.0, -200.0]));
+    /// assert!(aabb.intersect_point(Point2::new(50.0, 50.0)));
+    /// ```
+    pub fn intersect_point<V>(&self, point: V) -> bool
+    where
+        V: Into<Point2<f32>>,
+    {
+        let p = point.into();
+        p.x >= 0.0 && p.y >= 0.0 && p.x <= self.width && p.y <= self.height
     }
 }
