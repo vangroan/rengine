ab2cbca20fb04657b986bab6f84716d0dcf0e862
Willem Victor <wimpievictor@gmail.com>
2020-05-18 18:54:59 +0200

    Fiddled with mouse intersect test

--- src/gui/systems.rs
+++ src/gui/systems.rs
@@ -1,6 +1,8 @@
-use super::WidgetBounds;
+use super::{GuiGraph, WidgetBounds};
+use crate::comp::Transform;
+use crate::option::lift2;
 use glutin::{Event, WindowEvent};
-use specs::{Entities, Join, Read, ReadStorage, System};
+use specs::{Entities, Join, Read, ReadExpect, ReadStorage, System};
 
 pub struct GuiMouseMoveSystem;
 
@@ -8,24 +10,34 @@ impl<'a> System<'a> for GuiMouseMoveSystem {
     type SystemData = (
         Read<'a, Vec<Event>>,
         Entities<'a>,
+        ReadExpect<'a, GuiGraph>,
         ReadStorage<'a, WidgetBounds>,
+        ReadStorage<'a, Transform>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (events, entities, aabbs) = data;
+        let (events, entities, gui_graph, aabbs, transforms) = data;
 
         for ev in events.iter() {
             if let Event::WindowEvent { event, .. } = ev {
                 if let WindowEvent::CursorMoved { position, .. } = event {
-                    let (x, y) = (position.x as f32, position.y as f32);
-                    let (world_x, world_y) = (x / 1000.0, -y / 1000.0);
-                    for (entity, aabb) in (&entities, &aabbs).join() {
-                        if aabb.intersect_point([x, y]) {
-                            println!(
-                                "intersect ({}, {}) ({}, {}) {:?}",
-                                x, y, world_x, world_y, entity
-                            );
-                            break;
+                    let (mouse_x, mouse_y) = (position.x as f32, position.y as f32);
+                    let (world_x, world_y) = (mouse_x / 1000.0, -mouse_y / 1000.0);
+                    let mut walker = gui_graph.walk_dfs_post_order(gui_graph.root_id());
+
+                    while let Some(node_id) = walker.next(&gui_graph) {
+                        if let Some(entity) = gui_graph.get_entity(node_id) {
+                            let maybe_components = lift2(aabbs.get(entity), transforms.get(entity));
+                            if let Some((bounds, transform)) = maybe_components {
+                                // Bounds are in the widget's local space.
+                                if bounds.intersect_point([mouse_x, mouse_y]) {
+                                    println!(
+                                        "intersect ({}, {}) ({}, {}) {:?}",
+                                        mouse_x, mouse_y, world_x, world_y, entity
+                                    );
+                                    break;
+                                }
+                            }
                         }
                     }
                 }
